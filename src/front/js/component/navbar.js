f08c0a6320c330a4b5866dee2bd8d640164214ad
dianelabz <dbarboza@4geeks.co>
2022-05-17 01:32:56 +0000

    html listo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand">NoSepo</a>
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="¿Qué quieres ver?"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-light" type="submit">
+            🎞️
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
 };
